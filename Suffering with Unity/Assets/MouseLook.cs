49ebd3cac910580eb0787ad6caf91661a759fa4f
MGRessers <bigdream66111@gmail.com>
2020-12-31 15:01:44 +0500

    E01: Suffering with Unity - Movement

---
+++ Suffering with Unity/Assets/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float mouseSensitivity = 100f;
+
+	public Transform plrBody;
+
+	float xRotation = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float MouseX = Input.GetAxis ("Mouse X");
+		float MouseY = Input.GetAxis ("Mouse Y");
+
+		xRotation -= MouseY;
+		xRotation = Mathf.Clamp (xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler (xRotation, 0f, 0f);
+		plrBody.Rotate (Vector3.up * MouseX);
+
+	}
+}
---
+++ Suffering with Unity/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController cc;
+	public float speed = 12f;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		cc.Move (move * speed * Time.deltaTime);
+	}
+}
